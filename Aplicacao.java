3e147139b3dba00c8f4824040ebddeaa30f4219b
nashdenzel <32935737+nashdenzel@users.noreply.github.com>
2018-11-25 14:09:00 +0000

    Add files via upload

---
+++ Aplicacao.java
@@ -0,0 +1,70 @@
+
+package aplicacao;
+
+
+public class Aplicacao {
+      String registo_util;
+   
+}
+
+
+ class Licenciatura{
+    
+}
+
+class Mestrado{
+    
+}
+
+
+class Estudante {
+     String nome;
+     long id;
+}
+
+class Viagem{
+    
+
+}
+
+class Horario{
+    String horario_func;
+}
+
+class PontosInteresse{
+    String nome;
+    double despesas;
+    
+}
+
+
+class Museus extends PontosInteresse{
+ String descricao_tema;    
+}
+ class Parques extends PontosInteresse{
+
+}
+class Universidades extends PontosInteresse {
+    
+    
+}
+
+class Bares extends PontosInteresse{
+     String media;
+     
+}
+
+class Culturais extends Parques {
+    
+}
+
+class Diversão extends Parques{
+    
+}
+
+class Aquaticos{
+    String equipamentos;
+    String espet_animal;
+    
+}
+
